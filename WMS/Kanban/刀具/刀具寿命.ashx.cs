3f1f8f945fd187bb035e872b18bf2c22453e73db
ltt102858 <821449952@qq.com>
2020-07-21 15:37:05 +0800

    '修改刀具寿命'

--- WMS/Kanban/刀具/刀具寿命.ashx.cs
+++ WMS/Kanban/刀具/刀具寿命.ashx.cs
@@ -16,28 +16,27 @@ namespace WebApplication2.Kanban.刀具
            using(JDJS_WMS_DB_USEREntities entities=new JDJS_WMS_DB_USEREntities())
             {
                 var rows =
-                    from toolLife in entities.JDJS_WMS_ToolHolder_ToolLife_History_Table
-                    from toolShank in entities.JDJS_WMS_Tool_Shank_Table
-                    from tool in entities.JDJS_WMS_ToolHolder_Tool_Table
-                    from toolSpec in entities.JDJS_WMS_Tool_Stock_History
+                    from toolLife in entities.JDJS_WMS_Tool_LifeTime_Management
                     from cnc in entities.JDJS_WMS_Device_Info
                     from location in entities.JDJS_WMS_Location_Info
-                    where toolLife.ToolID == toolShank.ToolID & toolShank.CncID != null & tool.ID == toolShank.ToolID & tool.ToolSpecifications == toolSpec.Id&
-                    cnc.ID==toolShank.CncID&cnc.Position==location.id
+                    from shank in entities.JDJS_WMS_Tool_Shank_Table 
+                    from toolTable in entities.JDJS_WMS_ToolHolder_Tool_Table
+                    from toolS in entities.JDJS_WMS_Tool_Stock_History
+                    where toolLife.CncID == cnc.ID&cnc.Position ==location.id&&shank.CncID ==cnc.ID &&toolTable.ID ==shank.ToolID&&toolS.Id ==toolTable .ToolSpecifications 
                     select new
                     {
-                        toolShank.CncID,
-                        toolShank.ToolNum,
-                        toolSpec.KnifeName,
-                        toolSpec.KnifeSpecifications,
-                        toolLife.ToolCurrLife,
-                        toolLife.ToolMaxLife,
-                        cnc.MachNum,
-                        toolLife.Time,
+                        toolLife.CncID,
                         toolLife.ToolID,
+                        toolS.KnifeName,
+                        toolS.KnifeSpecifications,
+                        toolLife.ToolCurrTime,
+                        toolLife.ToolMaxTime,
+                        cnc.MachNum,
+                        //toolLife.Time,
+                        //toolLife.ToolID,
                         location.Name
                     };
-               var  rowsList = rows.OrderByDescending(r => r.Time).ToList();
+               var  rowsList = rows.OrderBy(r => (r.ToolMaxTime -r.ToolCurrTime)).ToList();
                 rowsList = rowsList.Where((x, i) => rowsList.FindIndex(p => p.ToolID == x.ToolID) == i).ToList();
 
                 var json = Serializer.Serialize(new { data= rowsList.Take(5) ,count = rowsList .Count});
